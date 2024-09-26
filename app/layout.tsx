450f1e0610566c8b726eb20a93a988e88983f806
Fahad <fahadkhann0204@gmail.com>
2024-09-26 22:43:24 +0530

    Prepare for 1st deployment try2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,28 @@
 /* eslint-disable camelcase */
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
-import '../styles/prism.css'
-import { Inter, Space_Grotesk } from 'next/font/google'
-import type { Metadata } from 'next'
 import React from 'react'
-import { ThemeProvider } from '@/context/ThemeProvider'
+import { ClerkProvider, SignedIn, SignedOut, SignInButton } from '@clerk/nextjs'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next';
 
-const inter = Inter(
-  {
-    subsets: ['latin'],
-    weight: ["100" , "200" , "300" , "400" , "500" , "600" , "700" , "800" , "900"],
-    variable: '--font-inter'
-  }
-)
+import './globals.css';
+import '../styles/prism.css';
+import { ThemeProvider } from '@/context/ThemeProvider';
 
-const spaceGrotesk = Space_Grotesk(
-  {
-    subsets: ['latin'],
-    weight: ["300", "400", "500", "600", "700"],
-    variable: '--font-spaceGrotesk'
-  }
-)
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
 
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+ 
 export const metadata: Metadata = {
   title: 'DevFlow',
-  description: 'A community-driven platform for asking and answering programming questions.',
+  description: 'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
   icons: {
     icon: '/assets/images/site-logo.svg'
   }
