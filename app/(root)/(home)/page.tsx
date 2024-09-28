abde36a824407484b513afb52eeead40223be759
Fahad <fahadkhann0204@gmail.com>
2024-09-28 14:17:22 +0530

    clerk webhook updated

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -15,7 +15,7 @@ import type { Metadata } from 'next';
 import { auth } from '@clerk/nextjs/server'
 
 export const metadata: Metadata = {
-  title: "Home | Dev Overflow"
+  title: "Home | Dev Stack"
 }
 
 
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -12,14 +12,14 @@ const Navbar = () => {
       <nav className='flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>
           <Link href="/" className='flex items-center gap-1'>
               <Image
-                src="/assets/images/site-logo.svg"
-                width={23}
-                height={23}
+                src="/assets/images/logo-min-2.svg"
+                width={30}
+                height={30}
                 alt='DevFlow'  
               />
 
               <p className='h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden'>Dev 
-                  <span className='text-primary-500'> Overflow</span>
+                  <span className='text-primary-500'> Stack</span>
               </p>
               </Link>
     
